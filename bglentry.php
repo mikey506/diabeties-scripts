22f0350ed5cb92ff6885f41c3b9508131fb5969c
Michael Landry <michaellandry3@hotmail.com>
2020-01-21 21:43:46 -0400

    Exploit still exists, but for private use its okay

--- bglentry.php
+++ bglentry.php
@@ -1,9 +1,11 @@
 <?php 
-  $content = $_POST['total']; 
-  if (strlen($content) < '160') {
-    $myfile = file_put_contents('BGLEntries.txt', $content.PHP_EOL , FILE_APPEND | LOCK_EX);
-    print('<a href="http://bbsn.ca/BGLEntries.txt"> View Entries </a><br><a href="http://bbsn.ca/bgltest.html"> Go Back </a>');
-} else {
+  $content = $_POST['total'];
+  $contstr = strlen($content); 
+  if ($constr > '160') {
     echo $content;
+} else {
+    $myfile = file_put_contents('BGLLog.html', $content.PHP_EOL , FILE_APPEND | LOCK_EX);
+    print('<a href="http://bbsn.ca/BGLLog.html"> View Entries </a><br><a href="http://bbsn.ca/bglcalc.html"> Go Back </a>');
 }
 ?>
+
