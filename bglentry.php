0141d6018a0c3acbb3b3a318e74741963aa226c5
Michael Landry <michaellandry3@hotmail.com>
2020-01-21 21:01:25 -0400

    Added a bit of security limiting the size of str

--- bglentry.php
+++ bglentry.php
@@ -1,5 +1,9 @@
 <?php 
-  $content = $_POST['total'];  
- $myfile = file_put_contents('BGLEntries.txt', $content.PHP_EOL , FILE_APPEND | LOCK_EX);
- print('<a href="http://bbsn.ca/BGLEntries.txt"> View Entries </a><br><a href="http://bbsn.ca/bgltest.html"> Go Back </a>');
+  $content = $_POST['total']; 
+  if (strlen($content) < '160') {
+    $myfile = file_put_contents('BGLEntries.txt', $content.PHP_EOL , FILE_APPEND | LOCK_EX);
+    print('<a href="http://bbsn.ca/BGLEntries.txt"> View Entries </a><br><a href="http://bbsn.ca/bgltest.html"> Go Back </a>');
+} else {
+    echo $content;
+}
 ?>
